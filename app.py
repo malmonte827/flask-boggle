f7c2e26adcde1b17fe9160b0c2b788e3cf106a4a
mike <malmonte827@gmail.comgit config --global user.name malmonte827dgit configqgit config --global user.email malmonte827@gmail.com>
2024-06-20 18:24:49 -0400

    Homepage made

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 from boggle import Boggle
-
+from flask import Flask, render_template, request, redirect, flash, session
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'very secret'
+
+
+@app.route('/')
+def homepage():
+    """ Displays Board"""
+    board = boggle_game.make_board()
+    session['board'] = board
+    return render_template('index.html', board=board)
+
+
